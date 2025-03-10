b645578d0a948a9c22d707e551bfbb928642b404
Nico4O4 <nicopfin@bluewin.ch>
2025-03-11 00:34:40 +0100

    hi

---
+++ integer.py
@@ -0,0 +1,38 @@
+#LANGSAM ARBEITEN
+
+# Print
+#print(4+1)
+
+#  Variablen
+#a  = 3
+#b = 7
+#print(a+b)
+
+# Mit Benutzer Eingabe + Variable
+
+#zahl = int(input("Gib eine Zahl ein: "))
+#print("Deine Zahl * 4 ist:", zahl * 4)
+
+# Auswählen zwischen + und -
+
+while True: 
+    rechner = int(input("Gib 1 für + und 2 für - ein: "))
+
+    if rechner == 1:
+        print("Du hast + gewählt")
+        zahl1 = int(input("Bitte gib die erste Zahl ein: "))
+        zahl2 = int(input("Bitte gib die zweite Zahl ein: "))
+        print("Das Ergebnis ist:", zahl1 + zahl2)
+
+    if rechner == 2:
+        print("Du hast - gewählt")
+        zahl1 = int(input("Bitte gib die erste Zahl ein: "))
+        zahl2 = int(input("Bitte gib die zweite Zahl ein: "))
+        print("Das Ergebnis ist:", zahl1 - zahl2)
+
+    print("Berechnung abgeschlossen was jetzt?")
+    option = input("Neue Berechnung starten? [Y/N]: ")
+    
+    if option == "N":
+        print("Programm beendet")
+        exit()
---
+++ main.py
@@ -0,0 +1,20 @@
+# Apple Teleporter Version 1.0
+# By: Apple Inc.
+# Date: 2042-01-03
+
+print("Welcome to the Apple Teleporter 1.0 by Apple Inc.")
+
+print("Please enter the meters you want to teleport in front of you.")
+input_meters = input("Meters:")
+
+print("Please enter how many people you want to transport with you.")
+input_persons = input("Persons:")
+
+starting_costs = 100
+costs_per_meter = 12
+costs_per_person = 14
+
+# Corrected cost calculation
+total_costs = starting_costs + (int(input_meters) * costs_per_meter) + (int(input_persons) * costs_per_person)
+
+print("The total costs for teleporting", input_meters, "meters with", input_persons, "persons is", total_costs, "Dollars")
