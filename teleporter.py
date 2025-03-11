3e506995e5e962c3ec8bcf79da392af55ec4c2d0
Nico4O4 <nicopfin@bluewin.ch>
2025-03-11 01:27:30 +0100

    teleporter

--- teleporter.py
+++ teleporter.py
@@ -2,7 +2,7 @@
 # By: Apple Inc.
 # Date: 2042-01-03
 
-print("Welcome to the Apple Teleporter 1.0 by Apple Inc.")
+print("Welcome to the Teleporter 1.0.")
 
 print("Please enter the meters you want to teleport in front of you.")
 input_meters = input("Meters:")
@@ -14,7 +14,7 @@ starting_costs = 100
 costs_per_meter = 12
 costs_per_person = 14
 
-# Corrected cost calculation
+
 total_costs = starting_costs + (int(input_meters) * costs_per_meter) + (int(input_persons) * costs_per_person)
 
-print("The total costs for teleporting", input_meters, "meters with", input_persons, "persons is", total_costs, "apple dollars.")
+print("The total costs for teleporting", input_meters, "meters with", input_persons, "persons is", total_costs, "dollars.")
